5f24e768b6e82eb409676bd6fca0a387838c3d95
harebsaeed <harebsaeed@email.com>
2026-06-09 15:29:04 +0400

    fix: add logger to redis_client.py so _request

--- bot/redis_client.py
+++ bot/redis_client.py
@@ -1,7 +1,10 @@
 import json
+import logging
 import time
 import httpx
 from bot.config import UPSTASH_REDIS_URL, UPSTASH_REDIS_TOKEN
+
+logger = logging.getLogger(__name__)
 
 
 class RedisClient:
