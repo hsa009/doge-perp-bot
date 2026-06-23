820d179d00fb20135061644faf1e99cfc5cb3c76
harebsaeed <harebsaeed@email.com>
2026-06-23 18:17:31 +0400

    keep AI_MODELS= in config for backward compat

--- bot/config.py
+++ bot/config.py
@@ -21,6 +21,7 @@ LEVERAGE = int(os.environ.get("LEVERAGE", "10"))
 MAX_DAILY_LOSS_USD = float(os.environ.get("MAX_DAILY_LOSS_USD", "2.0"))
 
 AI_LOOP_INTERVAL = int(os.environ.get("AI_LOOP_INTERVAL", "600"))
+AI_MODELS = ""
 
 ACTIVE_ASSET = os.environ.get("ACTIVE_ASSET", "DOGE")
 
